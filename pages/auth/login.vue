a934f35e2522af4416bb418c80fb5ab5cbb9d0f2
Alvin Faiz <softkopi3258@gmail.com>
2022-07-07 19:09:52 +0700

    add loading animation

--- pages/auth/login.vue
+++ pages/auth/login.vue
@@ -21,7 +21,11 @@
           <input type="password" name="password" id="password" placeholder="Password" required v-model="password">
         </label>
         
-        <button type="submit" class="w-full px-4 py-2 mt-4 text-white bg-purple-600 rounded hover:bg-purple-400">
+        <button type="submit" class="flex justify-center w-full px-4 py-2 mt-4 text-white bg-purple-600 rounded hover:bg-purple-400">
+          <svg class="hidden w-5 h-5 mr-3 -ml-1 text-white animate-spin" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" id="login-loading">
+            <circle class="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4"></circle>
+            <path class="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
+          </svg>
           Login
         </button>
         <span class="block w-full pb-4 mt-4 text-center">
@@ -67,13 +71,17 @@ export default {
   
   methods: {
     async login() {
-
+        var loading = document.querySelector('#login-loading')
+        loading.classList.add('animate-spin')
+        loading.classList.toggle('hidden')
       axios.post('https://virtserver.swaggerhub.com/restuarachman/Macaiki/1.0.0/login', {
         email: this.email,
         password: this.password
       })
       .then(res => {
         console.log(res)
+        loading.classList.remove('animate-spin')
+        loading.classList.toggle('hidden')
       })
     }
   }
