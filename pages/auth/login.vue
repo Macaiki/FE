4e88dbababda94af671af37ed09ddfe45a561f10
Alvin Faiz <softkopi3258@gmail.com>
2022-07-07 20:53:55 +0700

    bug tampilan

--- pages/auth/login.vue
+++ pages/auth/login.vue
@@ -2,13 +2,13 @@
   <div>
     <BackgroundLogin/>
     <div class="absolute items-center block w-full py-8 -translate-x-1/2 -translate-y-1/2 rounded-md top-1/2 left-1/2 md:max-w-2xl md:bg-zinc-700">
-      <div class="mx-8">
+      <div class="mx-8 text-center">
         <img src="~/assets/images/logo.png" alt="background" class="w-32 mb-4 md:mx-auto">
         <span class="text-xs text-white md:hidden">
           Login dulu yuk, agar bisa menikmati fitur-fitur Macaiki dengan nyaman.
         </span>
-        <span class="hidden text-xs text-white md:block">
-          Login dulu yuk, agar bisa menikmati fitur-fitur Macaiki dengan nyaman.
+        <span class="hidden text-purple-600 text-medium md:block">
+          Berbagi pengetahuan, cerita, hobi, dan pengalaman unik lainnya.
         </span>
       </div>
       <form class="mx-8 mt-12 border-gray-300 md:border-b" method="POST" @submit.prevent="login">
@@ -21,7 +21,7 @@
           <input type="password" name="password" id="password" placeholder="Password" required v-model="password">
         </label>
         
-        <button type="submit" class="flex justify-center w-full px-4 py-2 mt-4 text-white bg-purple-600 rounded hover:bg-purple-400">
+        <button type="submit" class="flex justify-center w-full px-4 py-2 mt-4 text-white bg-purple-600 rounded hover:bg-purple-400 md:py-4">
           <svg class="hidden w-5 h-5 mr-3 -ml-1 text-white animate-spin" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" id="login-loading">
             <circle class="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4"></circle>
             <path class="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
@@ -36,7 +36,7 @@
       </form>
       <div class="hidden px-8 md:block">
         <span class="block w-full text-center">
-          <router-link to="/auth/register" type="submit" class="w-full px-4 py-2 mt-4 text-white bg-purple-600 rounded hover:bg-purple-400">
+          <router-link to="/auth/register" type="submit" class="w-full px-4 py-2 mt-4 text-white border-2 border-purple-600 rounded hover:bg-purple-600 md:py-4">
               Register
           </router-link>
         </span>
