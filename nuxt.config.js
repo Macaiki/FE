2e74a86f9b333d05bff65b51e75914717aa77986
Alvin Faiz <softkopi3258@gmail.com>
2022-07-14 11:19:20 +0700

    remove proxy nuxt

--- nuxt.config.js
+++ nuxt.config.js
@@ -56,33 +56,9 @@ export default {
   // Axios module configuration: https://go.nuxtjs.dev/config-axios
   axios: {
     // Workaround to avoid enforcing hard-coded localhost:3000: https://github.com/nuxt-community/axios-module/issues/308
-    // baseURL: 'http://108.136.47.34:8080/api/v1',
-    baseURL: 'http://108.136.47.34:8080/api/v1',
-    browserBaseURL: 'http://108.136.47.34:8080/api/v1',
-    proxy: true,
-    prefix: 'http://108.136.47.34:8080/api/v1'
-  },
-  proxy: {
-    '/api': {
-      target: 'http://108.136.47.34:8080/api/v1',
-      pathRewrite: {  
-        '^/api': ''
-      },
-      secure: false,
-      changeOrigin: true
-    }
-  },
-  publicRuntimeConfig: {
-    axios: {
-      browserBaseURL: 'http://108.136.47.34:8080/api/v1'
-    }
+
   },
 
-  privateRuntimeConfig: {
-    axios: {
-      baseURL: 'http://108.136.47.34:8080/api/v1'
-    }
-  },
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {
--- store/register/actions.js
+++ store/register/actions.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 export default {
     handleRegister(store, param) {
-        let url = 'http://108.136.47.34:8080/api/v1/register';
+        let url = '/api/register';
         axios.post(url, param)
             .then(response => {
                 if(response.data.Meta.code === 200 && response.data.Meta.message === 'OK') {
